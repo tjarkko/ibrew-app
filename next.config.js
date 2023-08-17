fae71835adcf410b7d6934bf1e916857ed4b5e86
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-08-17 17:17:15 +0300

    Yet another trial for fixing CI build. Some changes needed due to using App routing.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {trailingSlash: true}
+const nextConfig = {
+  output: 'export',  
+  trailingSlash: true
+}
 
 module.exports = nextConfig
