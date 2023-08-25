f889d3ded111e8c843ee80065a9fb094bddd2e41
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-08-25 16:46:32 +0300

    Adding some logging to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({ pages: { signIn: '/login' } })
+//export default withAuth({ pages: { signIn: '/login' } })
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log('this is from middleware')
+    console.log(req.nextauth.token)
+  },
+  {
+    pages: { signIn: '/login' },
+  }
+)
 
 //export const config = { matcher: ['/dashboard'] }
 export const config = {
