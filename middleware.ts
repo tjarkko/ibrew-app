fceb38412f2465b7d9be8d2c00eaded091ccec7d
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-08-25 18:03:24 +0300

    Use default for trailingSlash setting and try a simpler matcher for the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-//import { withAuth } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
 
 //export default withAuth({ pages: { signIn: '/login' } })
 
-/*export default withAuth(
+export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
     console.log('this is from middleware')
@@ -11,31 +11,9 @@
   {
     pages: { signIn: '/login' },
   }
-)*/
+)
 
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  // Clone the request headers and set a new header `x-hello-from-middleware1`
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-hello-from-middleware1', 'hello')
-  console.log(`middleware, path: ${request.nextUrl.pathname}`)
-
-  // You can also set request headers in NextResponse.rewrite
-  const response = NextResponse.next({
-    request: {
-      // New request headers
-      headers: requestHeaders,
-    },
-  })
-
-  // Set a new response header `x-hello-from-middleware2`
-  response.headers.set('x-hello-from-middleware2', 'hello')
-  return response
+export const config = {
+  //matcher: ['/((?!login).*)'],
+  matcher: ['/about/:path*', '/dashboard/:path*'],
 }
-
-//export const config = { matcher: ['/dashboard'] }
-/*export const config = {
-  matcher: ['/((?!login).*)'],
-}*/
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  
-  trailingSlash: true
+  output: 'standalone',
+  //trailingSlash: true
 }
 
 module.exports = nextConfig
