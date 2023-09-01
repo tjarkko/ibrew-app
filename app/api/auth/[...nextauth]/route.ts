b4ca4537f4225236aa7bff5d5784b810f0a3374b
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-01 12:21:55 +0300

    Remove middleware for now

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const authOptions: AuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
