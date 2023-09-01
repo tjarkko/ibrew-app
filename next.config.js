ed66f59a182b9ba15c52dc4082dbd2b334b20647
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-01 16:22:59 +0300

    Try to make standalone build work in app service

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: 'standalone',
+  output: 'standalone',
   //trailingSlash: true
-  //distDir: 'build',
+  distDir: 'build',
 }
 
 module.exports = nextConfig
