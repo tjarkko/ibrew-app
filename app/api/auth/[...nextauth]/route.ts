cede6d33a2a4c5481c6870157742c89dbad0375f
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-01 13:13:26 +0300

    Fix ci build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
