03e0834b823ae3ec67f3f0971bf4704313ee0214
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-01 13:44:16 +0300

    Changes for deploying to app service

--- load-appinsights.js
+++ load-appinsights.js
@@ -1,20 +1,20 @@
 // https://medium.com/microsoftazure/enabling-the-node-js-application-insights-sdk-in-next-js-746762d92507
 
 const path = require('path')
-require('dotenv').config({path: path.resolve(process.cwd(), '.env.local')})
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') })
 
-let appInsights = require('applicationinsights');
+let appInsights = require('applicationinsights')
 appInsights
   .setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
   .setAutoCollectConsole(true, true)
-  .setAutoCollectDependencies(true)
+  .setAutoCollectDependencies(false)
   .setAutoCollectExceptions(true)
-  .setAutoCollectHeartbeat(true)
+  .setAutoCollectHeartbeat(false)
   .setAutoCollectPerformance(true, true)
   .setAutoCollectRequests(true)
   .setAutoDependencyCorrelation(true)
   .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-  .setSendLiveMetrics(true)
-  .setUseDiskRetryCaching(true);
+  .setSendLiveMetrics(false)
+  .setUseDiskRetryCaching(false)
 //appInsights.defaultClient.setAutoPopulateAzureProperties(true);
-appInsights.start();
+appInsights.start()
