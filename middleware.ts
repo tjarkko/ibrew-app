6cdb90e263041c5fdfa742817476e39355e098a1
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-06 11:34:02 +0300

    Add wrapper over the next auth middleware
    * Try to modify the 'origin' parameter so that next-auth would use external hostname for the redirect when not authenticated.

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,30 @@
-import { withAuth } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
 
-//export default withAuth({ pages: { signIn: '/login' } })
+const authMiddleware = withAuth({ pages: { signIn: '/login' } })
 
-export default withAuth(
+export function middleware(
+  request: NextRequestWithAuth,
+  _next: NextFetchEvent
+) {
+  console.log(`middleware, path: ${request.nextUrl.pathname}`)
+  console.log('request:')
+  console.log(request)
+  const req = {
+    ...request,
+    nextUrl: { ...request.nextUrl, origin: request.headers.get('host') },
+  }
+  console.log('req:')
+  console.log(request)
+  /*if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    console.log('redirect to login')
+    return NextResponse.redirect(new URL('/login', request.url))
+  }*/
+  return authMiddleware(request, _next)
+}
+
+/*export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
     console.log('this is from middleware')
@@ -14,7 +36,7 @@ export default withAuth(
   {
     pages: { signIn: '/login' },
   }
-)
+)*/
 
 export const config = {
   matcher: ['/((?!login).*)'],
