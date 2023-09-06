b4202860ca5a7cf59a0d2c97e739b3c061eb8f39
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2023-09-06 15:54:13 +0300

    Ugly hack to make the redirect use host value from headers
    * Next auth middleware uses the origin value when doing redirect to login page (when unauthenticated). When running in App Service this seems to be set to the internal ip address of the VM perhaps?
    * Not so sure if this in any way a good way to handle it, this might have some security implications.

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,235 @@
 import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { DomainLocale } from 'next/dist/server/config'
+import { NextURL } from 'next/dist/server/web/next-url'
+import { INTERNALS } from 'next/dist/server/web/spec-extension/request'
 import { NextResponse } from 'next/server'
 import type { NextFetchEvent, NextRequest } from 'next/server'
 
 const authMiddleware = withAuth({ pages: { signIn: '/login' } })
+
+//export declare const INTERNALS: unique symbol
+
+/*declare class NextRequestNew
+  extends NextRequest
+  implements NextRequestWithAuth
+{
+  constructor(input, init = {}) {
+    this[INTERNALS] = {
+      cookies: new _cookies.RequestCookies(this.headers),
+      geo: init.geo || {},
+      ip: init.ip,
+      nextUrl,
+      url: process.env.__NEXT_NO_MIDDLEWARE_URL_NORMALIZE
+        ? url
+        : nextUrl.toString(),
+    }
+  }
+}*/
 
 export function middleware(
   request: NextRequestWithAuth,
   _next: NextFetchEvent
 ) {
   console.log(`middleware, path: ${request.nextUrl.pathname}`)
-  console.log('request:')
-  console.log(request)
-  const req = {
-    ...request,
-    nextUrl: { ...request.nextUrl, origin: request.headers.get('host') },
+  //console.log('request:')
+  //console.log(request)
+  //request[INTERNALS].nextUrl.origin = 'heppa'
+
+  const nextUrl: NextURL & any = {
+    //[Internal]: {},
+    analyze: () => {
+      return
+    },
+
+    formatPathname: (value: string) => {
+      return ''
+    },
+
+    formatSearch: (value: string) => {
+      return ''
+    },
+    get buildId(): string | undefined {
+      return request.nextUrl.buildId
+    },
+
+    set buildId(buildId: string | undefined) {
+      request.nextUrl.buildId = buildId
+    },
+
+    get locale(): string {
+      return request.nextUrl.locale
+    },
+
+    set locale(locale: string) {
+      request.nextUrl.locale = locale
+    },
+
+    get defaultLocale(): string | undefined {
+      return request.nextUrl.defaultLocale
+    },
+
+    get domainLocale(): DomainLocale | undefined {
+      return request.nextUrl.domainLocale
+    },
+
+    get searchParams(): URLSearchParams {
+      return request.nextUrl.searchParams
+    },
+
+    get host(): string {
+      return request.nextUrl.host
+    },
+
+    set host(value: string) {
+      request.nextUrl.host = value
+    },
+
+    get hostname(): string {
+      return request.nextUrl.hostname
+    },
+
+    set hostname(value: string) {
+      request.nextUrl.hostname = value
+    },
+
+    get port(): string {
+      return request.nextUrl.port
+    },
+
+    set port(value: string) {
+      request.nextUrl.port = value
+    },
+
+    get protocol(): string {
+      return request.nextUrl.protocol
+    },
+
+    set protocol(value: string) {
+      request.nextUrl.protocol = value
+    },
+
+    get href(): string {
+      return request.nextUrl.href
+    },
+
+    set href(url: string) {
+      request.nextUrl.href = url
+    },
+
+    get origin(): string {
+      const url = request.nextUrl.protocol + '//' + request.headers.get('host')
+      //+ request.nextUrl.port
+      //+ request.nextUrl.pathname
+      console.log('url: ' + url)
+      return url || request.nextUrl.origin
+    },
+
+    get pathname(): string {
+      return request.nextUrl.pathname
+    },
+
+    set pathname(value: string) {
+      request.nextUrl.pathname = value
+    },
+
+    get hash(): string {
+      return request.nextUrl.hash
+    },
+
+    set hash(value: string) {
+      request.nextUrl.hash = value
+    },
+
+    get search(): string {
+      return request.nextUrl.search
+    },
+
+    set search(value: string) {
+      request.nextUrl.search = value
+    },
+
+    get password(): string {
+      return request.nextUrl.password
+    },
+
+    set password(value: string) {
+      request.nextUrl.password = value
+    },
+
+    get username(): string {
+      return request.nextUrl.username
+    },
+
+    set username(value: string) {
+      request.nextUrl.username = value
+    },
+
+    get basePath(): string {
+      return request.nextUrl.basePath
+    },
+
+    set basePath(value: string) {
+      request.nextUrl.basePath = value
+    },
+
+    toString(): string {
+      return request.nextUrl.toString()
+    },
+
+    toJSON(): string {
+      return request.nextUrl.toJSON()
+    },
+
+    clone(): NextURL {
+      return request.nextUrl.clone()
+    },
+    ['Internal']: undefined,
   }
-  console.log('req:')
-  console.log(request)
+
+  const req: NextRequestWithAuth = {
+    ...request,
+    [INTERNALS]: {
+      cookies: request.cookies,
+      geo: request.geo,
+      ip: request.ip,
+      url: request.url,
+      nextUrl: request.nextUrl,
+    },
+    get cookies() {
+      return this[INTERNALS].cookies
+    },
+    get geo() {
+      return this[INTERNALS].geo
+    },
+    get ip() {
+      return this[INTERNALS].ip
+    },
+    get nextUrl() {
+      /*const nextUrl: NextURL = {
+        ...this[INTERNALS].nextUrl,
+        origin: request.headers.get('host') || this[INTERNALS].nextUrl.origin,
+      }*/
+
+      return nextUrl
+    },
+    get page() {
+      return
+    },
+    get ua() {
+      return
+    },
+    get url() {
+      return this[INTERNALS].url
+    },
+  }
+  //console.log('req:')
+  //console.log(req)
+
   /*if (request.nextUrl.pathname.startsWith('/dashboard')) {
     console.log('redirect to login')
     return NextResponse.redirect(new URL('/login', request.url))
   }*/
-  return authMiddleware(request, _next)
+  return authMiddleware(req, _next)
 }
 
 /*export default withAuth(
