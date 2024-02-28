cd55b1bdda6d18e33a7d4bdd275ac6ec07926b9b
Jarkko Tuorila <126685740+tjarkko@users.noreply.github.com>
2024-02-28 16:51:17 +0200

    Get api url from env variable

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -29,7 +29,7 @@ export async function GET(req: NextRequestWithAuth) {
   const testJwt =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Imphcmtrb3R1b3JpbGEiLCJzdWIiOiJqYXJra290dW9yaWxhIiwianRpIjoiNTQ2NzdiZmMiLCJhdWQiOlsiaHR0cDovL2xvY2FsaG9zdDo2MjAyNCIsImh0dHBzOi8vbG9jYWxob3N0OjQ0MzE1IiwiaHR0cDovL2xvY2FsaG9zdDo1MDY4IiwiaHR0cHM6Ly9sb2NhbGhvc3Q6NzEyOCJdLCJuYmYiOjE2OTQ1OTAyNTMsImV4cCI6MTcwMjQ1MjY1MywiaWF0IjoxNjk0NTkwMjU0LCJpc3MiOiJkb3RuZXQtdXNlci1qd3RzIn0.UYFhcnbGDWhZDbWmS3HAdjuTrEH9ikB_2nuV9FluSX8'
 
-  const response = await fetch('http://localhost:5068/weatherforecast', {
+  const response = await fetch(`${process.env.IBREW_API}/weatherforecast`, {
     headers: {
       Authorization: `Bearer ${myJwt}`,
     },
